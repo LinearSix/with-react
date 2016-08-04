186926a82af470b7c6074852c76e8be6eb2aa776
Ryan Sobol <contact@ryansobol.com>
2016-08-04 10:17:10 -0700

    Tweak app

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const App = React.createClass({
   render() {
-    return <main>
+    return <div>
       <h2>Ryan Sobol's React skeleton for Brunch</h2>
       <ul>
         <li><a href="http://brunch.io">Brunch</a></li>
         <li><a href="https://facebook.github.io/react/">React.js</a></li>
       </ul>
-    </main>;
+    </div>;
   }
 });
 
